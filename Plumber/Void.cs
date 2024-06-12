9255285740efdce38ce5eb042f6af5634b2f21a0
Mark Lauter <mark@marklauter.com>
2024-06-11 23:43:38 -0400

    made void record struct

--- Plumber/Void.cs
+++ Plumber/Void.cs
@@ -7,5 +7,5 @@
 /// This type is useful for the TResponse generic parameter when the request scenario doesn't require a response.
 /// Examples include IRequestHandlerBuilder{string, Void}, RequestContext{string, Void}, etc.
 /// </remarks>
-public readonly struct Void { }
+public readonly record struct Void;
 
